e70d146146a80e108f1f9d106d87668742bc09f2
Asser-wael <aserweal@gmail.com>
2026-06-28 19:05:45 +0300

    Add React project

--- src/pages/adminPages/Dashboard.jsx
+++ src/pages/adminPages/Dashboard.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import { socket } from "../../services/socket";
 
 export default function Dashboard() {
+
+  const [onlineUsers, setOnlineUsers] = useState(0);
+
+  useEffect(() => {
+    socket.on("onlineUsers", (count) => {
+      setOnlineUsers(count);
+    });
+
+    return () => {
+      socket.off("onlineUsers");
+    };
+  }, []);
+
   return (
-    <div>Dashboard</div>
+    <h1>Online Users: {onlineUsers}</h1>
   )
 }
+
